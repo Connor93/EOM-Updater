9902f5bb34a56504f2c0346874438f61ef654b9f
Connor Fraser <connorfraser7@gmail.com>
2025-06-18 17:59:50 -0300

    updating app, giving it a theme. also removing the menu bar from the app

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: { nodeIntegration: true, contextIsolation: false }
   });
   win.loadFile('renderer/index.html');
